f1d7593a1e11ae648218996e32b74da5053ff8b5
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-04-27 19:53:08 -0300

    Classe contato: validando e-mail e exibindo endereço completo.

--- cadastro.php
+++ cadastro.php
@@ -3,7 +3,7 @@
 require_once 'autoload.php';
 
 $usuario = new App\Alura\Usuario($_POST['nome']);
-$contato = new App\Alura\Contato($_POST['email']);
+$contato = new App\Alura\Contato($_POST['email'], $_POST['endereco'], $_POST['cep']);
 
 ?>
 
@@ -28,8 +28,8 @@ $contato = new App\Alura\Contato($_POST['email']);
     <li class="list-group-item">Usuário: <?= $contato->getUsuario(); ?> </li>
     <li class="list-group-item">Senha: </li>
     <li class="list-group-item">Telefone: </li>
-    <li class="list-group-item">Email: </li>
-    <li class="list-group-item">Endereço: </li>
+    <li class="list-group-item">Email: <?= $contato->getEmail(); ?></li>
+    <li class="list-group-item">Endereço: <?= $contato->getEnderecoCep(); ?></li>
 </ul>
 </div>
 </body>
--- src/Alura/Contato.php
+++ src/Alura/Contato.php
@@ -5,8 +5,25 @@ namespace App\Alura;
 class Contato 
 {
     private $email;
+    private $endereco;
+    private $cep;
 
-    public function __construct(string $email)
+    public function __construct(string $email, string $endereco, string $cep)
+    {
+        $this->email = $email;
+
+        if ($this->validaEmail($email) !== false) 
+        {
+            $this->setEmail($email);
+        } else {
+            $this->setEmail("E-mail inválido.");
+        }
+
+        $this->endereco = $endereco;
+        $this->cep = $cep;
+    }
+
+    public function setEmail($email) 
     {
         $this->email = $email;
     }
@@ -19,5 +36,21 @@ class Contato
             return "Usuário inválido.";
         }
         return substr($this->email, 0, $posicaoArroba);
+    }
+
+    private function validaEmail(string $email)
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getEnderecoCep()
+    {
+        $enderecoCep = [$this->endereco, $this->cep];
+        return implode(" - ", $enderecoCep); // implode é o oposto de explode: ele junta os elementos de um array em uma única string, usando uma string de cola.
     }
 }
