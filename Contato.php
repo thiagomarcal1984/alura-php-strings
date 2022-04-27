5f521722cc01c5a5e2eae2a17d768e3bf57f777b
Thiago Marçal <thiagomarcal1984@gmail.com>
2022-04-27 11:42:07 -0300

    Classe Usuário: funções strpos e substr.

---
+++ Contato.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Alura;
+
+class Contato 
+{
+    private $email;
+
+    public function __construct(string $email)
+    {
+        $this->email = $email;
+    }
+
+    public function getUsuario() : string
+    {
+        $posicaoArroba = strpos($this->email, "@");
+        if ($posicaoArroba === false) 
+        {
+            return "Usuário inválido.";
+        }
+        return substr($this->email, 0, $posicaoArroba);
+    }
+}
